8b3956994cb9d64e4280774c4a02e2c401a67944
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-10-27 12:20:44 -0500

    changed to variables

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 let counter= 100;
 
-document.getElementById("before-prefix-Increment").innerHTML= counter;
-document.getElementById("prefixIncrement").innerHTML= ++counter;
+let prefixIncrement= ++counter;
+let postfixIncrement= counter++;
+let prefixDecrement= --counter;
+let postfixDecrement= counter--;
 
-document.getElementById("before-postfix-Increment").innerHTML= counter;
-document.getElementById("postfixIncrement").innerHTML= counter++;
-
-document.getElementById("before-prefix-Decrement").innerHTML= counter;
-document.getElementById("prefixDecrement").innerHTML= --counter;
-
-document.getElementById("before-postfix-Decrement").innerHTML= counter;
-document.getElementById("postfixDecrement").innerHTML= counter--;
 
 document.getElementById("after-postfix-Decrement").innerHTML= counter;
+
 
 
 
