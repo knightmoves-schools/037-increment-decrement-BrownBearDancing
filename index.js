4ee2b253c806373c1d4f10b41e7add1003edc082
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-10-27 12:13:12 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+let counter= 100;
+
+document.getElementById("before-prefix-Increment").innerHTML= count;
+document.getElementById("prefixIncrement").innerHTML= ++count;
+
+document.getElementById("before-postfix-Increment").innerHTML= count;
+document.getElementById("postfixIncrement").innerHTML= count++;
+
+document.getElementById("before-prefix-Decrement").innerHTML= count;
+document.getElementById("prefixDecrement").innerHTML= --count;
+
+document.getElementById("before-postfix-Decrement").innerHTML= count;
+document.getElementById("postfixDecrement").innerHTML= count--;
+
+document.getElementById("after-postfix-Decrement").innerHTML= counter;
+
+
+
 
