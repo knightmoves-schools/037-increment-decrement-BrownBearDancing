851bba912a4a75a0802ce5897101d4f66be1f602
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-10-27 12:16:16 -0500

    changed to counter

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 let counter= 100;
 
-document.getElementById("before-prefix-Increment").innerHTML= count;
-document.getElementById("prefixIncrement").innerHTML= ++count;
+document.getElementById("before-prefix-Increment").innerHTML= counter;
+document.getElementById("prefixIncrement").innerHTML= ++counter;
 
-document.getElementById("before-postfix-Increment").innerHTML= count;
-document.getElementById("postfixIncrement").innerHTML= count++;
+document.getElementById("before-postfix-Increment").innerHTML= counter;
+document.getElementById("postfixIncrement").innerHTML= counter++;
 
-document.getElementById("before-prefix-Decrement").innerHTML= count;
-document.getElementById("prefixDecrement").innerHTML= --count;
+document.getElementById("before-prefix-Decrement").innerHTML= counter;
+document.getElementById("prefixDecrement").innerHTML= --counter;
 
-document.getElementById("before-postfix-Decrement").innerHTML= count;
-document.getElementById("postfixDecrement").innerHTML= count--;
+document.getElementById("before-postfix-Decrement").innerHTML= counter;
+document.getElementById("postfixDecrement").innerHTML= counter--;
 
 document.getElementById("after-postfix-Decrement").innerHTML= counter;
+
 
 
 
